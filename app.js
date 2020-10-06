f2c8485e0581178d2b5f8106ea483deb764934e1
Muhammad Shahbaz <rssialvi@gmail.com>
2020-10-06 13:57:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-function letsPrint() {
-    console.log("It's finally working");
-
+const person = {
+    fname: 'Shahbaz',
+    lname: 'Shahbaz',
+    walk() {
+        console.log("Lets start walking");
+    },
+    Talk() {
+        console.log("nothing to talk");
+    }
 }
-letsPrint();
+const credential = 'fname';
+person[credential] = "Muhammad";
+console.log(person[credential] + " " + person['lname']);
+const walk = person.walk;
+walk();
